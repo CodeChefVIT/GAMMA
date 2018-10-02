e6aa91dc165bac545d4ca1e524b0f0c5827f1740
mayankshah1607 <mayankm.shah2017@vitstudent.ac.in>
2018-10-02 12:59:22 +0530

    Made APIp

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from textblob import TextBlob
 import numpy as np
 from rake_nltk import Rake
-
-
-
+from flask import request
+from flask_cors import CORS
+import flask
 
 def sentiment(sentence):
 	return TextBlob(sentence).polarity
@@ -32,14 +32,13 @@ def show_analysis(d):
 
 	for key,value in d.items():
 		print(key, ":", value)
-import flask
+
 def filter_questions(sentence):	
 	if TextBlob(sentence).tags[0][1] == 'WRB' or TextBlob(sentence).tags[0][1] == 'WP':
-		print('SHITBOT: I don\'t understand. Please answer as asked.')
+		print('I don\'t understand. Please answer as asked.')
 		return 1
 
-collected_data = {}
-
+collected_data = {}	
 questions = ["What do you feel about the teaching?",
 			"Your opinion about the course content?",
 			"Were we helpful during examinations?",
@@ -47,32 +46,40 @@ questions = ["What do you feel about the teaching?",
 			"Your opinion about the library?",
 			"What do you feel about extra-curricular?"]
 
+app = flask.Flask(__name__)
+CORS(app)
+
+@app.route("/chat",methods=["POST"])
 def converse():
-	for i in questions:
-		print("SHITBOT: ",i)
-		ans = input(">>")
-		while (filter_questions(ans)):
-			print("SHITBOT: ",i)
-			ans = input(">>")
-			print('inside loop')
 
-		if sentiment(ans) < 0 :
+	question = request.form.get('question')
+	print(question)
+	ans = input(">>")
+	if filter_questions(ans):
+		return flask.jsonify({'comment':'I don\'t understand. Please answer as asked.',
+			'question':question})
 
-			reply = reply_negative()
-			print(reply)
-			additional_ans = input(">>")
+	if sentiment(ans) < 0 :
 
-			collected_data[additional_ans] = get_keywords(additional_ans)
+		reply = reply_negative()
+		additional_ans = input(">>")
+		collected_data[additional_ans] = get_keywords(additional_ans)
 
-		else :
+	else :
 
-			reply = reply_positive()
-			print(reply)
-
+		reply = reply_positive()
+	data = {'reply':reply, sentiment: sentiment(ans)}
+	return flask.jsonify(data)
 
 
-converse()
-print(collected_data)
+if __name__ == '__main__':
+	app.run(host='localhost',port=5000)
+
+
+				
+
+
+
 
 
 
